d6c81ef269ce8b0d5c953c19077f312725ae447d
HackSmith010 <anshujaiswal119@gmail.com>
2025-07-15 21:52:56 +0530

    battery feature ongoing

--- jarvis_battery_automation/battery_integration.py
+++ jarvis_battery_automation/battery_integration.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+from jarvis_battery_automation.battery_alert import *
+from jarvis_battery_automation.battery_plug_check import *
+from check_battery_percent import *
 
 def battery_cmd(query):
     if "check battery percentage" in query or "battery percentage" in query:
-        check_battery
+        battery_percentage()
